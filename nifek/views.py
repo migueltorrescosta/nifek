044a42ee59bb5a632c6c79de9fcf20366bd9f4f4
migueltorrescosta <miguelptcosta1995@gmail.com>
2022-12-22 14:04:22 +0000

    Smooth out homepage

--- nifek/views.py
+++ nifek/views.py
@@ -4,9 +4,5 @@ from django.shortcuts import render
 def home(request):
     """View function for home page of site."""
 
-    context = {
-        "thes_tags": ["forecasting", "truth", "accountability"],
-    }
-
     # Render the HTML template index.html with the data in the context variable
-    return render(request, "home.html", context=context)
+    return render(request, "home.html", context={})
