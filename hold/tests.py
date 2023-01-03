60b12232ab346bed3dbf5e43dc0bf13ad2b02f5f
migueltorrescosta <miguelptcosta1995@gmail.com>
2023-01-03 21:21:16 +0000

    Fix tests

--- hold/tests.py
+++ hold/tests.py
@@ -48,6 +48,7 @@ class HoldTestCase(TestCase):
         data = {
             "name": "Entity Created During Tests",
             "description": "mocked description",
+            "wealth": 14.6 * 10**9,
         }
         self.client.force_login(user=self.user)
         response = self.client.post(relative_url, follow=True, data=data)
