cd16f86226fa0e1870438d0597425bf989401a0f
migueltorrescosta <miguelptcosta1995@gmail.com>
2023-03-08 20:32:53 +0000

    Add bootstrap4 dependencies

--- nifek/settings.py
+++ nifek/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.postgres",
+    "crispy_bootstrap4",
     "crispy_forms",
     "magiclink",
     "accounts",
